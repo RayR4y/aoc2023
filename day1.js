bf4a7d6515504ede0d6327d7014400da044945b5
Kathrin Schmitt <RayR4y@web.de>
2023-12-29 09:26:03 +0100

    reading input file works

--- day1.js
+++ day1.js
@@ -1,1 +1,13 @@
+//const stream = new ReadableStream("day1input.txt");
+//const fileReader = new FileReader()
+const fs = require('fs');
+fs.readFile('day1input.txt', (err, data) => {
+    if (err) throw err;
+   
+    console.log(data.toString());
+  });
 
+//function readfile(){
+    //const text = FileReader.readAsText("day1input.txt")
+    //console.log(text);
+//}
