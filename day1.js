57c5acdcdcfd163bc697d918feca050c7af1f67d
Kathrin Schmitt <RayR4y@web.de>
2023-12-29 10:05:33 +0100

    implementation seems to work

--- day1.js
+++ day1.js
@@ -3,8 +3,21 @@
 const fs = require('fs');
 fs.readFile('day1input.txt', (err, data) => {
     if (err) throw err;
-   
-    console.log(data.toString());
+    const input = data.toString().split('\n');
+    let sum = 0;
+    for(let i = 0; i<input.length; i++){
+            const regex = /[1-9]/g;
+            const found = input[i].match(regex);
+            if(found!= null){
+                const num = found[0] + found[found.length-1]
+                console.log(num)
+                sum = sum + Number(num)
+                //console.log(Number(num)+1)
+            }
+            
+    }
+    console.log(sum)
+    //console.log(input[2].match(regex));
   });
 
 //function readfile(){
