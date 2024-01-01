ea850817ddd2e4b105c5bd7355ee1768a59618d3
Kathrin Schmitt <RayR4y@web.de>
2024-01-01 13:41:27 +0100

    Bugfixing strange bug that not always recognizes the string comparison right

--- day2/puzzle2/day2.js
+++ day2/puzzle2/day2.js
@@ -14,27 +14,38 @@ fs.readFile('day2input.txt', (err, data) => {
         let redcubesmin = 0
         let greencubesmin = 0
         let bluecubesmin = 0
-        for(let r=0; r<revealedSets.length;r++){
+        for(let r=1; r<revealedSets.length;r++){
            revealedSet = revealedSets[r].split(' ') 
-           if(revealedSet[2]==='red' && revealedSet[1]>redcubesmin){
+           console.log('revealed set before: ' + revealedSets[r])
+           console.log('revealed set before [1]' + revealedSet[1])
+           console.log('revealed set before [2]' + revealedSet[2])
+           console.log(revealedSet[2].toString())===('red')
+           console.log(Number(revealedSet[1])>redcubesmin)
+           if((revealedSet[2].toString())===('red') && Number(revealedSet[1])>redcubesmin){
              redcubesmin = revealedSet[1] 
+             console.log('[1]' + revealedSet[1])
+             console.log('[2]' + revealedSet[2])
+             console.log(redcubesmin)
            }
            else{
-            if(revealedSet[2]==='green' && revealedSet[1]>greencubesmin){
+            if(revealedSet[2]==='green' && Number(revealedSet[1])>greencubesmin){
                 greencubesmin = revealedSet[1]  
               }
               else{
-                if(revealedSet[2]==='blue' && revealedSet[1]>bluecubesmin){
+                if(revealedSet[2]==='blue' && Number(revealedSet[1])>bluecubesmin){
                     bluecubesmin = revealedSet[1] 
                   }
               }
            } 
            console.log('revealed set: ' + revealedSets[r])
+           console.log('revealed set [1]' + revealedSet[1])
+           console.log('revealed set [2]' + revealedSet[2])
         }
         console.log('minimum cubes: red - ' + redcubesmin + ',green - ' + greencubesmin + ',blue - ' + bluecubesmin)
         let power = redcubesmin * greencubesmin * bluecubesmin
         console.log(i)
         console.log(input[i])
+        console.log('power:' + power)
         sum = sum + power
     }
     console.log(sum)
