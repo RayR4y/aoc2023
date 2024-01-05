bbfb5ed768b64c9fd729d60254c9a5c21ffa86eb
Kathrin Schmitt <RayR4y@web.de>
2024-01-05 09:07:16 +0100

    day5 puzzle one seems to work now

--- day5/day5.js
+++ day5/day5.js
@@ -23,16 +23,26 @@ fs.readFile('day5input.txt', (err, data) => {
     }
 
     for(let i = 0; i<mappings.length; i++){
+        console.log(actualNumbers + ' ' + i)
         for(let a = 1; a<actualNumbers.length; a++){
             matchfound = false;
             console.log(actualNumbers[a])
             actualNumbers[a] = Number(actualNumbers[a])
             for(let j = 0; j<mappings[i].length; j++){
-                dataSet = mappings[i][j]
+                console.log('Mappings ij:')
+                console.log(mappings[i][j])
+                dataSet = mappings[i][j].split(' ')
                 if(matchfound == false){
+                    console.log('actualnum: ' + actualNumbers[a])
+                    console.log('dataSet[0]: ' + dataSet[0])
+                    console.log('dataSet[1]: ' + dataSet[1])
+                    console.log('dataSet[2]: ' + dataSet[2])
+                    console.log('dataSet:' + dataSet )
                     if(Number(actualNumbers[a]) >= Number(dataSet[1]) && Number(actualNumbers[a])<= (Number(dataSet[1]) + Number(dataSet[2]))){
                         matchfound = true;
                         //acutalNumbers[i] =  m[j][c][0] + (actualNumbers[i]-m[j][c][1)
+                        let newnum = Number(dataSet[0]) + (Number(actualNumbers[a]) - Number(dataSet[1]));
+                        console.log('newnum:' + newnum)
                         actualNumbers[a] = Number(dataSet[0]) + (Number(actualNumbers[a]) - Number(dataSet[1]))
                     }
 
