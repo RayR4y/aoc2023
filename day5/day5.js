b18d1f1ab35e2ad7f0bfc82a019b72fc864b6cb3
Kathrin Schmitt <RayR4y@web.de>
2024-01-05 08:53:13 +0100

    day5 puzzle 1 still gives the wrong result

--- day5/day5.js
+++ day5/day5.js
@@ -8,27 +8,45 @@ fs.readFile('day5input.txt', (err, data) => {
     const regex2 = /[a-z]/g;
     mappings = [[],[],[],[],[],[],[]]
     seeds = input[0].split(' ');
+    actualNumbers = input[0].split(' ');
     counter = 0;
-    min = 0;
     for(let i = 3; i<input.length; i++){
-        //console.log(mappings[counter])
         if(input[i].match(regex)!=null){
-            //console.log(mappings[0])
             mappings[counter].push(input[i])
         }
         else{
             if(input[i].match(regex2)==null){
-                console.log(mappings[counter])
+                //console.log(mappings[counter])
                 counter = counter+1 
             }
         }
     }
 
-    for(let i = 0; i<7; i++){
-        for(let j = 0; j<mappings[i].length; j++){
-            console.log(mappings[i][j])
+    for(let i = 0; i<mappings.length; i++){
+        for(let a = 1; a<actualNumbers.length; a++){
+            matchfound = false;
+            console.log(actualNumbers[a])
+            actualNumbers[a] = Number(actualNumbers[a])
+            for(let j = 0; j<mappings[i].length; j++){
+                dataSet = mappings[i][j]
+                if(matchfound == false){
+                    if(Number(actualNumbers[a]) >= Number(dataSet[1]) && Number(actualNumbers[a])<= (Number(dataSet[1]) + Number(dataSet[2]))){
+                        matchfound = true;
+                        //acutalNumbers[i] =  m[j][c][0] + (actualNumbers[i]-m[j][c][1)
+                        actualNumbers[a] = Number(dataSet[0]) + (Number(actualNumbers[a]) - Number(dataSet[1]))
+                    }
+
+                }
+            }
+
         }
     }
+    allFinalnumbers = []
+    for(let b = 1; b<actualNumbers.length;b++){
+        allFinalnumbers.push(actualNumbers[b])
+    }
+    min = Math.min(...allFinalnumbers)
+    console.log('min: ' + min)
 
     
     
