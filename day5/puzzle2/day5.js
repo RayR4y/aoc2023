2f2b5a53c69ba56d51f88df501304ee57a44e211
Kathrin Schmitt <RayR4y@web.de>
2024-01-05 10:32:56 +0100

    waiting for result would take to long because of codestructure and size of numbers

--- day5/puzzle2/day5.js
+++ day5/puzzle2/day5.js
@@ -51,36 +51,37 @@ fs.readFile('day5input.txt', (err, data) => {
     actualNumbers.push(Number(initialNumbers[e]))
 }*/
 for(let r = 0; r<maxrange; r++){
-    console.log('old actual Numbers:')
-    console.log(actualNumbers)
+    console.log('r:  ' + r + '  /  ' + maxrange)
+    //console.log('old actual Numbers:')
+    //console.log(actualNumbers)
     for(let e = 0; e<initialNumbers.length;e++){
         if(Number(initialNumbers[e]) + Number(ranges[e]) > Number(initialNumbers[e])+r){
             actualNumbers[e] = Number(initialNumbers[e])+r;
         }
     }
-    console.log('new actual Numbers:')
-    console.log(actualNumbers)
+    //console.log('new actual Numbers:')
+    //console.log(actualNumbers)
     for(let i = 0; i<mappings.length; i++){
-        console.log(actualNumbers + ' ' + i)
+        //console.log(actualNumbers + ' ' + i)
         for(let a = 0; a<actualNumbers.length; a++){
             matchfound = false;
-            console.log(actualNumbers[a])
+            //console.log(actualNumbers[a])
             actualNumbers[a] = Number(actualNumbers[a])
             for(let j = 0; j<mappings[i].length; j++){
-                console.log('Mappings ij:')
-                console.log(mappings[i][j])
+                //console.log('Mappings ij:')
+                //console.log(mappings[i][j])
                 dataSet = mappings[i][j].split(' ')
                 if(matchfound == false){
-                    console.log('actualnum: ' + actualNumbers[a])
-                    console.log('dataSet[0]: ' + dataSet[0])
-                    console.log('dataSet[1]: ' + dataSet[1])
-                    console.log('dataSet[2]: ' + dataSet[2])
-                    console.log('dataSet:' + dataSet )
+                    //console.log('actualnum: ' + actualNumbers[a])
+                    //console.log('dataSet[0]: ' + dataSet[0])
+                    //console.log('dataSet[1]: ' + dataSet[1])
+                    //console.log('dataSet[2]: ' + dataSet[2])
+                    //console.log('dataSet:' + dataSet )
                     if(Number(actualNumbers[a]) >= Number(dataSet[1]) && Number(actualNumbers[a])<= (Number(dataSet[1]) + Number(dataSet[2]))){
                         matchfound = true;
                         //acutalNumbers[i] =  m[j][c][0] + (actualNumbers[i]-m[j][c][1)
-                        let newnum = Number(dataSet[0]) + (Number(actualNumbers[a]) - Number(dataSet[1]));
-                        console.log('newnum:' + newnum)
+                        //let newnum = Number(dataSet[0]) + (Number(actualNumbers[a]) - Number(dataSet[1]));
+                        //console.log('newnum:' + newnum)
                         actualNumbers[a] = Number(dataSet[0]) + (Number(actualNumbers[a]) - Number(dataSet[1]))
                     }
 
