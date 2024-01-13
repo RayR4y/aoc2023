1cd2b5f6bee530b91fc4353b0cb5b4a27decff96
Kathrin Schmitt <RayR4y@web.de>
2024-01-13 20:17:27 +0100

    day6 puzzle1 seems to work

--- day6/day6.js
+++ day6/day6.js
@@ -1,4 +1,41 @@
 const fs = require('fs');
 fs.readFile('day6input.txt', (err, data) => {
-    
+    if (err) throw err;
+    const input = data.toString().split('\n')
+    console.log(input)
+    timestemp = input[0].split(' ')
+    recordstemp = input[1].split(' ')
+    console.log(recordstemp)
+    times = []
+    records = []
+    amountOfwaystowin = []
+    for(let i = 1; i<recordstemp.length; i++){
+      if(recordstemp[i] != ''){
+        records.push(Number(recordstemp[i]))
+      }
+    }
+    for(let i = 1; i<timestemp.length; i++){
+      if(timestemp[i] != ''){
+        times.push(Number(timestemp[i]))
+      }
+    }
+    console.log(times)
+    console.log(records)
+    for(let i = 0; i<times.length; i++){
+      let counter = 0
+      for(let j = 1; j<times[i]; j++){
+        timebuttonhold = j;
+        traveltime = times[i]-j
+        result = timebuttonhold*traveltime;
+        if(result>records[i]){
+          counter = counter+1
+        }
+      }
+      amountOfwaystowin.push(counter)
+    }
+    mul = 1;
+    for(let i = 0; i<amountOfwaystowin.length; i++){
+      mul = mul * amountOfwaystowin[i]
+    }
+    console.log(mul)
   });
